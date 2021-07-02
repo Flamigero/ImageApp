1c2bd0b32886688f4ea6d15782e5913a3d40d01c
Flama <carlosnjimenez97@gmail.com>
2021-07-02 08:46:20 +0200

    Validate token middleware

---
+++ config/middlewares.py
@@ -0,0 +1,27 @@
+"""Custom Middlewares"""
+
+# Django
+from django.http import JsonResponse
+
+# JWT
+from imageapp.helpers.Jwt import JWT
+
+class ValidateToken:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        path = request.path
+        pathPost = 'posts'
+        
+        if path.find(pathPost) != -1:
+            token = request.META.get('HTTP_X_TOKEN')
+            if token:
+                check = JWT.validateJWT(token)
+                if(not check): return JsonResponse({"error": "Invalid token"}, status=400)
+                
+            else:
+                return JsonResponse({"error": "There aren't token"}, status=400)
+
+        return response
--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'config.middlewares.ValidateToken'
 ]
 
 ROOT_URLCONF = 'config.urls'
